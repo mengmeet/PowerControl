3eae95ef289182858b7e27fc5e1df574eb6a6e4a
honjow <honjow311@gmail.com>
2023-12-06 13:11:44 +0800

    update install scripts

--- install.sh
+++ install.sh
@@ -6,25 +6,45 @@ if ! [ -x "$(command -v jq)" ]; then
   exit 1
 fi
 
+temp=$(mktemp -d)
+
 # Download latest release
 RELEASE=$(curl -s 'https://api.github.com/repos/mengmeet/PowerControl/releases/latest')
+MESSAGE=$(echo "$RELEASE" | jq -r '.message')
+
+if [[ "$MESSAGE" != "null" ]]; then
+  echo "$MESSAGE" >&2
+  exit 1
+fi
+
 RELEASE_VERSION=$(echo "$RELEASE" | jq -r '.tag_name')
 RELEASE_URL=$(echo "$RELEASE" | jq -r '.assets[0].browser_download_url')
-curl -L -o /tmp/PowerControl.tar.gz "$RELEASE_URL"
+
+if [ -z "$RELEASE_VERSION" ] || [ -z "$RELEASE_URL" ]; then
+  echo "Failed to get latest release info" >&2
+  exit 1
+fi
+
+
+curl -L -o ${temp}/PowerControl.tar.gz "${github_prefix}${RELEASE_URL}"
 
 echo "Installing PowerControl $RELEASE_VERSION"
 
-# remove old version
-chmod -R 777 ${HOME}/homebrew/plugins
-rm -rf ${HOME}/homebrew/plugins/PowerControl
+if [ ! -f ${temp}/PowerControl.tar.gz ]; then
+  echo "Failed to download PowerControl $RELEASE_VERSION" >&2
+  exit 1
+fi
 
-# Extract
-tar -xzf /tmp/PowerControl.tar.gz -C ${HOME}/homebrew/plugins
+# remove old version
+chmod +w ${HOME}/homebrew/plugins
+chmod -R +w ${HOME}/homebrew/plugins/PowerControl
+
+rm -rf ${HOME}/homebrew/plugins/PowerControl || true
+
+# Extract and restart plugin_loader
+tar -xzf ${temp}/PowerControl.tar.gz -C ${HOME}/homebrew/plugins && sudo systemctl restart plugin_loader.service
 
 # Cleanup
-rm -f /tmp/PowerControl.tar.gz
+rm -f ${temp}/PowerControl.tar.gz
 
 echo "PowerControl $RELEASE_VERSION installed"
-
-# restart plugin_loader
-sudo systemctl restart plugin_loader.service
