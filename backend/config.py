96611eaa4a4ff0e93108770fb5eba343c8964c76
honjow <honjow311@gmail.com>
2024-04-17 22:18:24 +0800

    verify yaml file

--- backend/config.py
+++ backend/config.py
@@ -1,36 +1,38 @@
+import json
 import logging
 import glob
 import yaml
 import decky_plugin
 from helpers import get_homebrew_path
 
-#日志配置
+# 日志配置
 LOG_LOCATION = "/tmp/PowerControl_py.log"
 logging.basicConfig(
-    level = logging.INFO,
-    filename = LOG_LOCATION,
+    level=logging.INFO,
+    filename=LOG_LOCATION,
     format="[%(asctime)s | %(filename)s:%(lineno)s:%(funcName)s] %(levelname)s: %(message)s",
-    filemode = 'w',
-    force = True)
+    filemode="w",
+    force=True,
+)
 
-#路径配置
+# 路径配置
 try:
     HOMEBREW_PATH = get_homebrew_path()
     DECKY_PLUGIN_DIR = decky_plugin.DECKY_PLUGIN_DIR
-    SH_PATH="{}/backend/sh_tools.sh".format(DECKY_PLUGIN_DIR)
-    RYZENADJ_PATH="{}/bin/ryzenadj".format(DECKY_PLUGIN_DIR)
+    SH_PATH = "{}/backend/sh_tools.sh".format(DECKY_PLUGIN_DIR)
+    RYZENADJ_PATH = "{}/bin/ryzenadj".format(DECKY_PLUGIN_DIR)
     GPU_DEVICE_PATH = glob.glob("/sys/class/drm/card?/device")[0]
     GPUFREQ_PATH = "{}/pp_od_clk_voltage".format(GPU_DEVICE_PATH)
     GPULEVEL_PATH = "{}/power_dpm_force_performance_level".format(GPU_DEVICE_PATH)
-    PLATFORM_PROFILE_PATH  = '/sys/firmware/acpi/platform_profile'
-    PLATFORM_PROFILE_CHOICES_PATH = '/sys/firmware/acpi/platform_profile_choices'
+    PLATFORM_PROFILE_PATH = "/sys/firmware/acpi/platform_profile"
+    PLATFORM_PROFILE_CHOICES_PATH = "/sys/firmware/acpi/platform_profile_choices"
 
     FAN_HWMON_CONFIG_DIR = f"{DECKY_PLUGIN_DIR}/backend/fan_config/hwmon"
     FAN_EC_CONFIG_DIR = f"{DECKY_PLUGIN_DIR}/backend/fan_config/ec"
 except Exception as e:
     logging.error(f"路径配置异常|{e}")
 
-#设备信息获取配置
+# 设备信息获取配置
 try:
     cpuinfo_path = "/proc/cpuinfo"
     cpuinfo = open(cpuinfo_path, "r").read()
@@ -44,45 +46,48 @@ API_URL = "https://api.github.com/repos/mengmeet/PowerControl/releases/latest"
 
 CONFIG_KEY = "PowerControl"
 
-#TDP上限配置
+# 是否验证 yml 配置文件
+VERIFY_YML = False
+
+# TDP上限配置
 try:
-    TDP_LIMIT_CONFIG_PRODUCT={
-        "AIR":18,
-        "AIR 1S":25,
-        "AIR 1S Limited":20,
-        "AIR Pro":20,
-        "AIR Plus":30,
-        "AYANEO 2":30,
-        "GEEK":30,
-        "GEEK 1S":30,
-        "AYANEO 2S":30,
-        "ONEXPLAYER Mini":30,
-        "NEXT":35,
-        "ONEXPLAYER Mini Pro":40,
-        "AOKZOE A1 AR07":40,
-        "ONEXPLAYER 2 ARP23":45,
-        "ONEXPLAYER F1":35,
-        "ONEXPLAYER F1 EVA-01":35,
-        "G1619-04":45,     #GPD WINMAX2
-        "G1618-04":45,     #GPD WIN4
-        "G1617-01":30,    #GPD WIN mini
-        "ROG Ally RC71L_RC71L":30,
-        "ROG Ally RC71L":30,
-        "Jupiter":20,
+    TDP_LIMIT_CONFIG_PRODUCT = {
+        "AIR": 18,
+        "AIR 1S": 25,
+        "AIR 1S Limited": 20,
+        "AIR Pro": 20,
+        "AIR Plus": 30,
+        "AYANEO 2": 30,
+        "GEEK": 30,
+        "GEEK 1S": 30,
+        "AYANEO 2S": 30,
+        "ONEXPLAYER Mini": 30,
+        "NEXT": 35,
+        "ONEXPLAYER Mini Pro": 40,
+        "AOKZOE A1 AR07": 40,
+        "ONEXPLAYER 2 ARP23": 45,
+        "ONEXPLAYER F1": 35,
+        "ONEXPLAYER F1 EVA-01": 35,
+        "G1619-04": 45,  # GPD WINMAX2
+        "G1618-04": 45,  # GPD WIN4
+        "G1617-01": 30,  # GPD WIN mini
+        "ROG Ally RC71L_RC71L": 30,
+        "ROG Ally RC71L": 30,
+        "Jupiter": 20,
     }
-    TDP_LIMIT_CONFIG_CPU={
-        "7735HS":65,
-        "7735U":40,
-        "7735":45,
-        "5560U":18,
-        "5700U":28,
-        "5800U":30,
-        "5825U":30,
-        "6800U":40,
-        "4800U":25,
-        "4500U":25,
-        "3050e":12,
-        "Z1 Extreme":40,
+    TDP_LIMIT_CONFIG_CPU = {
+        "7735HS": 65,
+        "7735U": 40,
+        "7735": 45,
+        "5560U": 18,
+        "5700U": 28,
+        "5800U": 30,
+        "5825U": 30,
+        "6800U": 40,
+        "4800U": 25,
+        "4500U": 25,
+        "3050e": 12,
+        "Z1 Extreme": 40,
         "7840HS": 65,
         "7840U": 40,
         "7640U": 30,
@@ -95,44 +100,63 @@ try:
 except Exception as e:
     logging.error(f"TDP配置异常|{e}")
 
-#风扇配置
+
+def load_yaml(file_path: str, chk_schema=None) -> dict:
+    with open(file_path, "r") as f:
+        data = yaml.safe_load(f)
+
+    # 使用 schema 验证配置文件
+    if chk_schema is not None and VERIFY_YML:
+        try:
+            from jsonschema import validate
+
+            try:
+                validate(instance=data, schema=chk_schema)
+            except Exception as e:
+                logging.error(f"配置文件 {file_path} 验证失败 | {e}")
+                return {}
+        except Exception as e:
+            logging.error(f"验证模块导入失败, 跳过检查 | {e}")
+    return data
+
+
+# 风扇配置
 try:
 
     hwmon_configs = glob.glob(f"{FAN_HWMON_CONFIG_DIR}/*.yml")
     ec_configs = glob.glob(f"{FAN_EC_CONFIG_DIR}/*.yml")
 
+    hwmon_schema_path = f"{DECKY_PLUGIN_DIR}/backend/fan_config/schema/hwmon.json"
+    ec_schema_path = f"{DECKY_PLUGIN_DIR}/backend/fan_config/schema/ec.json"
+
+    with open(hwmon_schema_path, "r") as f:
+        hwmon_schema = json.load(f)
+    with open(ec_schema_path, "r") as f:
+        ec_schema = json.load(f)
+
     FAN_HWMON_LIST = {}
     for config in hwmon_configs:
-        with open(config, "r") as f:
-            data = yaml.safe_load(f)
-            name = data['hwmon_name']
-            if not name is None:
-                FAN_HWMON_LIST[name] = data['fans']
+        data = load_yaml(config, hwmon_schema)
+        if data == {}:
+            continue
+        name = data["hwmon_name"]
+        if not name is None:
+            FAN_HWMON_LIST[name] = data["fans"]
     # logging.info(f"FAN_HWMON_LIST: {FAN_HWMON_LIST}")
 
     FAN_EC_CONFIG_MAP = {}
     for config in ec_configs:
-        with open(config, "r") as f:
-            data = yaml.safe_load(f)
-            names = data['product_name']
-            if not name is None:
-                for name in names:
-                    FAN_EC_CONFIG_MAP[name] = data['fans']
+        data = load_yaml(config, ec_schema)
+        if data == {}:
+            continue
+        names = data["product_name"]
+        if not names is None:
+            for name in names:
+                FAN_EC_CONFIG_MAP[name] = data["fans"]
 
     FAN_EC_CONFIG = FAN_EC_CONFIG_MAP.get(PRODUCT_NAME, [])
     logging.info(f"FAN_EC_CONFIG: {FAN_EC_CONFIG}")
-    # FAN_EC_CONFIG = [{
-    #         "ram_reg_addr":0x4E,
-    #         "ram_reg_data":0x4F,
-    #         "ram_manual_offset":0x47A,
-    #         "ram_rpmwrite_offset":0x47A,
-    #         "ram_rpmread_offset":0x478,
-    #         "ram_rpmread_length":2,
 
-    #         "rpm_write_max":244,
-    #         "rpm_value_max":6700
-    #     }]
-    # logging.info(f"FAN_EC_CONFIG: {FAN_EC_CONFIG}")
-    
+
 except Exception as e:
     logging.error(f"风扇配置异常|{e}")
