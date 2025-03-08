dbae06217efab840e49a4855701ebde56a648839
honjow <honjow311@gmail.com>
2025-03-09 04:39:20 +0800

    read default curve from backend

--- src/util/backend.ts
+++ src/util/backend.ts
@@ -1,9 +1,10 @@
 import { APPLYTYPE, FANMODE, GPUMODE, Patch } from "./enum";
 import { FanControl, PluginManager } from "./pluginMain";
-import { Settings, SettingsData } from "./settings";
+import { FanSetting, Settings, SettingsData } from "./settings";
 import {
   DEFAULT_TDP_MAX,
   DEFAULT_TDP_MIN,
+  FanPosition,
   QAMPatch,
   SteamUtils,
   SystemInfo,
@@ -256,6 +257,43 @@ export class BackendData {
       return this.fanConfigs?.[index]?.fan_hwmon_mode ?? 0;
     }
     return 0;
+  }
+
+  public getHwmonMode1AutoVal(
+    index: number
+  ): { speed_value: number; temp_value: number }[] {
+    const result: { speed_value: number; temp_value: number }[] = [];
+    if (this.has_fanConfigs) {
+      const autoVals = this.fanConfigs?.[index]?.hwmon_mode1_auto_val ?? [];
+      const pwmWriteMax: number =
+        this.fanConfigs?.[index]?.pwm_write_max ?? 255;
+      console.log(">>>>>>>>>>> getHwmonMode1AutoVal", autoVals);
+      if (autoVals instanceof Array && autoVals.length > 0) {
+        for (let i = 0; i < autoVals.length; i++) {
+          const pwm_value = autoVals[i]?.pwm_write_value;
+          const temp_value = autoVals[i]?.temp_write_value;
+          if (pwm_value !== undefined && temp_value !== undefined) {
+            result.push({
+              speed_value: (pwm_value / pwmWriteMax) * 100, // pwm_value 转为百分比
+              temp_value,
+            });
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  public getHwmonAutoFanSetting(index: number): FanSetting | undefined {
+    const defaultPoints = this.getHwmonMode1AutoVal(index);
+    if (defaultPoints.length > 0) {
+      const curvePoints: FanPosition[] = defaultPoints.map(
+        (point) => new FanPosition(point.temp_value, point.speed_value)
+      );
+      console.log(">>>>>>>>>> getHwmonAutoFanSetting", curvePoints);
+      return new FanSetting(false, FANMODE.CURVE, 50, curvePoints);
+    }
+    return undefined;
   }
 
   public getCurrentVersion() {
