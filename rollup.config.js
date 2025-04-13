bb5f1683d93faafbcf2c5a168cc60df7bc956704
honjow <honjow311@gmail.com>
2025-04-13 19:11:19 +0800

    chore: Bump version to 2.13.3 and add merge-anything dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,12 @@
 import deckyPlugin from "@decky/rollup";
+import { mergeAndConcat } from "merge-anything";
 
-export default deckyPlugin({
+const userOptions = {
   // Add your extra Rollup options here
-})
+  treeshake: {
+    preset: "recommended",
+  },
+};
+
+// must be merged in the order of default options first, then user options
+export default mergeAndConcat(deckyPlugin(), userOptions);
