042b739bb1e9205ec7f6409bff8da9c8c29c2ece
honjow <honjow311@gmail.com>
2025-04-16 12:15:57 +0800

    feat: Implement platform profile management in LenovoDevice class

--- py_modules/devices/lenovo/lenovo_device.py
+++ py_modules/devices/lenovo/lenovo_device.py
@@ -11,10 +11,53 @@ LENOVO_WIM_FAST_PATH = f"{LENOVO_WIM_PATH}/ppt_pl3_fppt/current_value"
 LENOVO_WIM_SLOW_PATH = f"{LENOVO_WIM_PATH}/ppt_pl2_sppt/current_value"
 LENOVO_WIM_STAPM_PATH = f"{LENOVO_WIM_PATH}/ppt_pl1_spl/current_value"
 
+PLATFORM_PROFILE_PREFIX = "/sys/class/platform-profile"
+HWMON_PREFIX = "/sys/class/hwmon"
+
+PLATFORM_PROFILE_NAME = "lenovo-wmi-gamezone"
+SUGGESTED_DEFAULT = "custom"
+
 
 class LenovoDevice(PowerDevice):
     def __init__(self) -> None:
         super().__init__()
+
+    def find_sysdir(self, prefix: str, name: str) -> str:
+        for dir in os.listdir(prefix):
+            base_path = os.path.join(prefix, dir)
+            if os.path.exists(os.path.join(base_path, "name")):
+                with open(os.path.join(base_path, "name"), "r") as f:
+                    if f.read().strip() == name:
+                        return base_path
+        return None
+
+    def find_hwmon(self, name: str) -> str:
+        return self.find_sysdir(HWMON_PREFIX, name)
+
+    def find_platform_profile(self, name: str) -> str:
+        return self.find_sysdir(PLATFORM_PROFILE_PREFIX, name)
+
+    def get_platform_profile(self, name: str) -> str:
+        base_path = self.find_platform_profile(name)
+        try:
+            with open(os.path.join(base_path, "profile"), "r") as f:
+                return f.read().strip()
+        except FileNotFoundError:
+            logger.error(f"Platform profile {name} not found")
+            return None
+        except Exception as e:
+            logger.error(f"Failed to get platform profile {name}: {e}")
+            return None
+
+    def set_platform_profile(self, name: str, profile: str) -> None:
+        base_path = self.find_platform_profile(name)
+        try:
+            with open(os.path.join(base_path, "profile"), "w") as f:
+                f.write(profile)
+        except FileNotFoundError:
+            logger.error(f"Platform profile {name} not found")
+        except Exception as e:
+            logger.error(f"Failed to set platform profile {name}: {e}")
 
     def set_tdp(self, tdp: int) -> None:
         logger.debug(f"Setting TDP to {tdp}")
@@ -22,6 +65,9 @@ class LenovoDevice(PowerDevice):
             logger.info("TDP is too low, use default tdp method")
             super().set_tdp(tdp)
             return
+
+        self.set_platform_profile(PLATFORM_PROFILE_NAME, SUGGESTED_DEFAULT)
+
         fast_val = tdp + 2
         slow_val = tdp
         stapm_val = tdp
